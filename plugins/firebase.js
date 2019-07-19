fbe608cf8086019003c1ee592bb29920fb00c0f1
Hellden <r.chapotin@gmail.com>
2019-07-19 16:27:38 +0200

    update

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,9 +1,11 @@
+/* ------- import data firebase product -> FIRESTORE, AUTH ------- */
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
 if (!firebase.apps.length) {
   const config = {
+    // It's just for example, do not use. Replace with your data Firebase
     apiKey: 'AIzaSyDD4IhWUUFWOQz69V_L9X100DpCCwk-Ml0',
     authDomain: 'nuxt-project-raph.firebaseapp.com',
     databaseURL: 'https://nuxt-project-raph.firebaseio.com',
