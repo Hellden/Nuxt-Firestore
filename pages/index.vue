8dada5f01564189f416c72a479074aff7c3dfef7
Hellden <r.chapotin@gmail.com>
2019-07-26 10:16:17 +0200

    add bulma css

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div class="header">
-      <h1 class="header__title">v2 DAL + Firestore + Authentificaiton</h1>
+      <h1 class="header__title title">v2 DAL + Firestore + Authentificaiton</h1>
     </div>
 
-    <div>
-      <h2>Listing Firestore</h2>
+    <div class="container">
+      <h2 class="subtitle">Listing Firestore</h2>
       <ul>
         <li v-for="item in data" :key="item.index">
           {{ item.data().Nom }}<br />
