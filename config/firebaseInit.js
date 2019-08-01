998c5503eb748407f1e68e3f3131fcbece06dab3
Hellden <r.chapotin@gmail.com>
2019-08-01 14:00:57 +0200

    add coment

--- config/firebaseInit.js
+++ config/firebaseInit.js
@@ -2,18 +2,19 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 
-if (!firebase.apps.length) {
-  const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGE_SENDER_ID
-  }
-  firebase.initializeApp(firebaseConfig)
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID
 }
+// An issue commonly seen with Firebase Authentication is an error telling us an app already exist. The line below solves this issue.
+// eslint-disable-next-line no-unused-expressions
+!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : ''
 
 export const auth = firebase.auth()
 export const db = firebase.firestore()
 export const user = firebase.auth().currentUser
+export default firebase
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import { db, auth, user } from '@/config/firebaseInit'
 
+// Init prototype for using in all the application.
+// Using $firestore,$auth, $user
 const firebasePlugin = {
   install() {
-    if (!Vue.prototype.$db) {
-      Vue.prototype.$firestore = db
-      Vue.prototype.$auth = auth
-      Vue.prototype.$user = user
-    }
+    Vue.prototype.$firestore = db
+    Vue.prototype.$auth = auth
+    Vue.prototype.$user = user
   }
 }
 
